a30933f97b2db92da7dab503bcf7e33906ed4010
dch9610 <dch9610@naver.com>
2025-04-10 15:04:28 +0900

    Header 위젯 추가

--- lib/screen/question/question_screen.dart
+++ lib/screen/question/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_web/util/asset_path.dart';
+import 'package:flutter_responsive_web/widgets/header.dart';
 import 'package:flutter_responsive_web/widgets/menu/menu.dart';
 import 'package:flutter_responsive_web/widgets/page_drawer.dart';
 import 'package:flutter_responsive_web/widgets/screen_layout_builder.dart';
@@ -18,7 +20,15 @@ class _QuestionScreenState extends State<QuestionScreen> {
         return Scaffold(
           endDrawer: PageDrawer(currentIndex: 3, tablet: tablet),
           body: Column(
-            children: [Menu(currentIndex: 3, screenModel: screenModel)],
+            children: [
+              Menu(currentIndex: 3, screenModel: screenModel),
+              Header(
+                title: '온라인 문의',
+                subTitle: '프로젝트 및 서비스 문의를 남겨주세요.',
+                backgroundImage: AssetPath.questionHeaderImage,
+                screenModel: screenModel,
+              ),
+            ],
           ),
         );
       },
--- lib/util/asset_path.dart
+++ lib/util/asset_path.dart
@@ -1,4 +1,5 @@
 class AssetPath{
   static const menuLogo = "assets/logo/DKSYSTEMS.png";
   static const hamburger = "assets/images/hamburger.png";
+  static const questionHeaderImage = "assets/images/question_header_image.png";
 }
